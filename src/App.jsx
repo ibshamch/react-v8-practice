4a615494d2d1580ed14d739fa8a96e187c65fafe
Ibsham Chaudhary <progwithibi@gmail.com>
2023-11-11 02:35:06 +0500

    hooks

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import Pet from "./Pet";
+import { createRoot } from "react-dom/client";
+import SearchParams from "./SearchParams";
+
+const root = document.querySelector("#root");
+const container = createRoot(root);
+container.render(<App />);
+
+function App() {
+  return (
+    <div>
+      <SearchParams />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Pet.jsx
@@ -0,0 +1,10 @@
+const Pet = ({ name, breed, animal }) => {
+  return (
+    <div>
+      <h1>{name}</h1>
+      <h2>{animal}</h2>
+      <h2>{breed}</h2>
+    </div>
+  );
+};
+export default Pet;
